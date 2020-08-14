4d17b07834ed053e0108fd5896a90edf90c3ce95
Ünal KAYMAK <unal.kaymak@ibb.gov.tr>
2020-08-14 16:48:14 +0300

    router infrastructure

--- src/components/root/App.js
+++ src/components/root/App.js
@@ -1,16 +1,21 @@
-import React from 'react';
-import Navi from '../navi/Navi';
-import { Container } from 'reactstrap';
-import Dashboard from './Dashboard';
+import React from "react";
+import Navi from "../navi/Navi";
+import { Container } from "reactstrap";
+import Dashboard from "./Dashboard";
+import { Switch, Route } from "react-router-dom";
+import CartDetail from "../cart/CartDetail";
 
 function App() {
   return (
     <div>
       <Container>
-          <Navi/>
-          <Dashboard/>
+        <Navi />
+        <Switch>
+          <Route path="/" exact component={Dashboard} />
+          <Route path="/product" exact component={Dashboard} />
+          <Route path="/cart" exact component={CartDetail} />
+        </Switch>
       </Container>
-      
     </div>
   );
 }
