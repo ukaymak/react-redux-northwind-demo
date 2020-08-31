83a5294704d0e038071edcde486087cc365391fa
ukaymak <unalkaymak@hotmail.com>
2020-08-31 16:32:18 +0300

    show cart detail

--- src/components/cart/CartDetail.js
+++ src/components/cart/CartDetail.js
@@ -2,10 +2,50 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
 import * as cartActions from "../../redux/actions/cartActions";
+import { Table, Button } from "reactstrap";
+import alertify from "alertifyjs";
+
 
 class CartDetail extends Component {
+  removeFromCart(product) {
+    this.props.actions.removeFromCart(product);
+    alertify.error(product.productName + " product has been removed ");
+
+  }
+
   render() {
-    return <div/>;
+    return (
+      <div>
+        <Table striped>
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Prodcut Name</th>
+              <th>Unit Price</th>
+              <th>Quantity</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.props.cart.map((cartItem) => (
+              <tr key={cartItem.product.id}>
+                <td>{cartItem.product.id}</td>
+                <td>{cartItem.product.productName}</td>
+                <td>{cartItem.product.unitPrice}</td>
+                <td>{cartItem.quantity}</td>
+                <td>
+                  <Button
+                    color="danger"
+                    onClick={() => this.removeFromCart(cartItem.product)}
+                  >
+                    Sil
+                  </Button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+      </div>
+    );
   }
 }
 
