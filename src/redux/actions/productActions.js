2cde34070e9903467a7c206ede109897bd0cc15c
ukaymak <unalkaymak@hotmail.com>
2020-09-02 14:59:33 +0300

    error management

--- src/redux/actions/productActions.js
+++ src/redux/actions/productActions.js
@@ -7,11 +7,59 @@ export function getProductsSuccess(products) {
 export function getProducts(selectedCategoryID) {
   return function (dispatch) {
     let url = "http://localhost:3000/products";
-    if(selectedCategoryID){
-        url = url + "?categoryId="+selectedCategoryID
+    if (selectedCategoryID) {
+      url = url + "?categoryId=" + selectedCategoryID;
     }
     return fetch(url).then((response) =>
       response.json().then((result) => dispatch(getProductsSuccess(result)))
     );
   };
+}
+
+export function createProductSuccess(product) {
+  return { type: actionTypes.CREATE_PRODUCT_SUCCESS, payload: product };
+}
+
+export function updateProductSuccess(product) {
+  return { type: actionTypes.UPDATE_PRODUCT_SUCCESS, payload: product };
+}
+
+export function saveProductApi(product) {
+  //id yollarsak güncelleme, yoksa eklemedir
+
+  return fetch("http://localhost:3000/products/" + (product.id || ""), {
+    method: product.id ? "PUT" : "POST",
+    headers: { "content-type": "application/json" },
+    body: JSON.stringify(product),
+  })
+    .then(handleResponse)
+    .catch(handleError);
+}
+
+export function saveProduct(product) {
+  return function (dispatch) {
+    return saveProductApi(product)
+      .then((savedProduct) => {
+        product.id
+          ? dispatch(updateProductSuccess(savedProduct))
+          : dispatch(createProductSuccess(savedProduct));
+      })
+      .catch((error) => {
+        throw error;
+      });
+  };
+}
+
+export async function handleResponse(response){
+  if(response.ok){
+    return response.json()
+  }
+
+  const error = await response.text()
+  throw new Error(error)
+}
+
+export function handleError(error){
+  console.error("Bir hata olustu")
+  throw error;
 }
