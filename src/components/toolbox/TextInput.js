658e53043a5b66d3cee83e185f6fe09fb0ed93c0
ukaymak <unalkaymak@hotmail.com>
2020-09-03 14:59:40 +0300

    start to hooks with textInput component example

---
+++ src/components/toolbox/TextInput.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+const TextInput = ({ name, label, onChange, placeholder, value, error }) => {
+  let wrapperClass = "form-group";
+  if (error && error.lenght > 0) {
+    wrapperClass += " has-error";
+  }
+
+  return (
+    <div className={wrapperClass}>
+      <label htmlFor={name} />
+      <div className="field">
+        <input
+          type="text"
+          name={name}
+          className="form-control"
+          placeholder={placeholder}
+          value={value}
+          onChange={onchange}
+        />
+        {error&&<div className="alert alert-danger">{error}</div>}
+      </div>
+    </div>
+  );
+};
+
+export default TextInput;
